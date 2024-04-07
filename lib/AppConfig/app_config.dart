5f4779ef0d9d11dea36a6b55ad10025b0ba94c9b
shemeer <developers@wazeefa.in>
2024-04-07 13:06:44 +0530

    App theme color update

--- lib/AppConfig/app_config.dart
+++ lib/AppConfig/app_config.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppConfig {
   // static const String hostUrl = 'https://spn21.spondan.com/amazcart';
-  // static const String hostUrl = 'https://demo.wazeefa.in/cam';
-  static const String hostUrl = 'https://cam.tikkabahraini.com';
+  static const String hostUrl = 'https://demo.wazeefa.in/cam';
+  // static const String hostUrl = 'https://cam.tikkabahraini.com';
+  // static const String hostUrl = 'https://ezviz.com';
 
   static String appName = 'Ezviz';
 
@@ -15,14 +16,20 @@ class AppConfig {
 
   static const String assetPath = hostUrl + '/public';
 
-  static Color loginScreenBackgroundGradient1 = Color(0xffFF3566);
+  // static Color loginScreenBackgroundGradient1 = Color(0xffFF3566);
+  static Color loginScreenBackgroundGradient1 = Color(0xff549cff);
 
-  static Color loginScreenBackgroundGradient2 = Color(0xffD7365C);
+  // static Color loginScreenBackgroundGradient2 = Color(0xffD7365C);
+  static Color loginScreenBackgroundGradient2 = Color(0xff549cff);
 
-  static String appColorScheme = "#FF3364";
+  // static String appColorScheme = "#FF3364";
+  static String appColorScheme = "#549cff";
+
+  // static const String privacyPolicyUrl =
+  //     'https://spn21.spondan.com/amazcart/privacy-policy';
 
   static const String privacyPolicyUrl =
-      'https://spn21.spondan.com/amazcart/privacy-policy';
+      'https://demo.wazeefa.in/cam/privacy-policy';
 
   static bool googleLogin = false;
 
